47936b2ee0fe14239e277a68c705eaf62a321377
Ferenci Ákos Sándor <synthetace@gmail.com>
2024-08-17 22:36:05 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -1,29 +1,19 @@
 { 
-  description = "Julia ScientificFHS flake";
+  description = "Julia env flake";
 
   inputs = {
+    nixgl.url = "github:nix-community/nixGL";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     scientific-fhs.url =  "github:olynch/scientific-fhs";
   };
-  outputs = { self, nixpkgs, scientific-fhs }:
+  outputs = { self, nixgl, nixpkgs, scientific-fhs }:
     let 
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system; 
-        overlays = [];
+        overlays = [nixgl.overlay];
       };
     in {
-      imports = [scientific-fhs.nixosModules.default];
-      programs.scientific-fhs = {
-        enable = true;
-        juliaVersions = [
-          {
-            version = "julia_18";
-            default = true;
-          }
-        ];
-        enableNVIDIA = false;
-      };
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           julia-bin
